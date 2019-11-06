bbe5c05964bafc4e0d077dab3ad64963a251fcc7
Alex Banks <alex.banx98@gmail.com>
2019-11-06 12:36:00 +0000

    readd nan line

--- catkin_ws/src/pf_localisation/src/pf_localisation/pf.py
+++ catkin_ws/src/pf_localisation/src/pf_localisation/pf.py
@@ -78,6 +78,8 @@ class PFLocaliser(PFLocaliserBase):
 
          """
 
+        scan.ranges = map(lambda x: self.scan_range_max if math.isnan(x) else x, scan.ranges)
+        
         particlecloud = PoseArray()
         previous = 0
         cumulative = []
