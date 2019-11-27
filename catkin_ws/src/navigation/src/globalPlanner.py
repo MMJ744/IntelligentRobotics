79d12dd6bc3f9dbe4e573566013ed4d80ee7bbee
Matthew Jones <mattymjones@gmail.com>
2019-11-27 23:22:38 +0000

    working just need to subscribe and publish

--- catkin_ws/src/navigation/src/globalPlanner.py
+++ catkin_ws/src/navigation/src/globalPlanner.py
@@ -7,14 +7,14 @@ from nav_msgs.msg import Odometry, OccupancyGrid, Path#, GetPlan
 import tf
 
 class Node():
-    def __init__(self, x=0, y=0, parent=None):
+    def __init__(self, x=0, y=0, parent=None, cost=1):
         self.parent = parent
         self.x = x
         self.y = y
         self.g = 0
         self.h = 0
         self.f = 0
-
+        self.cost = cost
     def __eq__(self,other):
         return self.x == other.x and self.y == other.y
 
@@ -25,7 +25,8 @@ resolution = 0.05
 
 def valid(x,y):
     global costmap
-    return costmap[x][y] == 0
+    global width
+    return costmap[y*width + x] == 0
 
 
 def getNeighbours(n):
@@ -33,26 +34,27 @@ def getNeighbours(n):
     global height
     global width
     neighbours = []
+    diagcost = 1.5
     x = n.x
     y = n.y
     if x > 0:
-        if costmap[x-1][y]==0:
+        if valid(x-1,y):
             neighbours.append(Node(x-1,y))
         if y > 0:
-            if costmap[x-1][y-1]==0:
-                neighbours.append(Node(x-1,y-1))
-        if y < height-1 and costmap[x-1][y+1]==0:
-            neighbours.append(Node(x-1,y+1))
+            if valid(x-1,y-1):
+                neighbours.append(Node(x-1,y-1, cost=diagcost))
+        if y < height-1 and valid(x-1,y+1):
+            neighbours.append(Node(x-1,y+1, cost=diagcost))
     if x < width-1:
-        if y > 0 and costmap[x+1][y-1]==0:
-            neighbours.append(Node(x+1,y-1))
-        if y < height-1 and costmap[x+1][y+1]==0:
-            neighbours.append(Node(x+1,y+1))
-        if costmap[x+1][y]==0:
+        if y > 0 and valid(x+1,y-1):
+            neighbours.append(Node(x+1,y-1, cost=diagcost))
+        if y < height-1 and valid(x+1,y+1):
+            neighbours.append(Node(x+1,y+1, cost=diagcost))
+        if valid(x+1,y):
             neighbours.append(Node(x+1,y))
-    if y > 0 and costmap[x][y-1]==0:
+    if y > 0 and valid(x,y-1):
         neighbours.append(Node(x,y-1))
-    if y < height-1 and costmap[x][y+1]==0:
+    if y < height-1 and valid(x,y+1):
         neighbours.append(Node(x,y+1))
     return neighbours
 
@@ -62,7 +64,7 @@ def h(current, goal):
 
 
 def findPath(costmap, startx, starty, goalx, goaly):
-    if(costmap[startx][starty]==1 or costmap[startx][starty==1]):
+    if not (valid(startx,starty) and valid(goalx,goaly)):
         print("start or goal not in map")
         return None
     path = []
@@ -82,36 +84,44 @@ def findPath(costmap, startx, starty, goalx, goaly):
         closedList.append(current)
         if current==goalNode:
             while current is not None:
-                path.append((current.x,current.y))
+                pose = PoseStamped()
+                pose.pose.position.x = current.x
+                pose.pose.position.y = current.y
+                path.append(pose)
                 current = current.parent
             break
         for neighbour in getNeighbours(current):
             if neighbour in closedList:
                 continue
-            neighbour.g = current.g + 1
+            neighbour.g = current.g + neighbour.cost
             neighbour.h = h(neighbour, goalNode)
             neighbour.f = neighbour.g + neighbour.h
             neighbour.parent = current
             for n in openList:
-                if n == neighbour and n.f <= neighbour.f:
+                if n == neighbour and n.g <= neighbour.g:
                     continue
             openList.append(neighbour)
     return path[::-1]
 
 def test():
     global costmap
-    costmap = [[0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
-              [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
-    print(findPath(costmap,0,0,1,8))
+    costmap = [0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 1, 0, 0, 0, 0, 0,0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
+    print(findPath(costmap,3,0,5,0))
+    print(valid(4,0))
+    print(valid(4,1))
+    print(valid(4,5))
 
+def mapcallback(data):
+    pass
+    
+
+def main():
+    rospy.Subscriber("/map", OccupancyGrid, mapcallback)
+    rospy.Subscriber("/odom", Odometry, odomCallback)
+    rospy.Subscriber("sonar", SonarArray, sonarCallback)
+    rospy.init_node('Global Planner', anonymous=True)
 
 
 if __name__ == '__main__':
