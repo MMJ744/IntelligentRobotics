2108b0328adb401ae107277e03319170a5be7ae0
Brett Saunders <bjs730@student.bham.ac.uk>
2019-11-28 22:43:33 +0000

    Fixed State error

--- catkin_ws/src/speach/src/State.py
+++ catkin_ws/src/speach/src/State.py
@@ -1,5 +1,5 @@
 class State:
     def run(self):
-        asert 0, "run not implemented"
+        assert 0, "run not implemented"
     def next(self, input):
         assert 0, "next not implemented"
