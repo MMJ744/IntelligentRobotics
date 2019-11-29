3330b4b77c974709e872c5591929b8fc5c8b92d2
Matthew Jones <mattymjones@gmail.com>
2019-11-29 23:04:45 +0000

    web interface -> navigation working

--- catkin_ws/src/speach/src/WebCommunicator.py
+++ catkin_ws/src/speach/src/WebCommunicator.py
@@ -1,8 +1,8 @@
+#!/usr/bin/env python
 from flask import Flask
 from flask import request
+import navcall
 
-import rospy
-from std_msgs.msg import String
 app = Flask(__name__)
 
 locs = {
@@ -22,10 +22,9 @@ def hello():
 
 @app.route('/goto')
 def goto():
-    rospy.init_node('web', anonymous=True)
-    pub = rospy.Publisher('navIn',String,queue_size=10)
-    pub.publish(request.args.get('location', ''))
+    navcall.call(request.args.get('location',''))
     return 'going to ' + request.args.get('location', '') + str(locs[request.args.get('location', '')])
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0:5000')
+    navcall.main()
+    app.run(host='0.0.0.0')
