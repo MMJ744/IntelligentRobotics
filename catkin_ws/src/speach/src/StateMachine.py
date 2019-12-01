22dfdeebb66ca99bada82e537b63f2688e188c91
Brett Saunders <bjs730@student.bham.ac.uk>
2019-12-01 01:07:00 +0000

    Bug fixes and working speech

--- catkin_ws/src/speach/src/StateMachine.py
+++ catkin_ws/src/speach/src/StateMachine.py
@@ -2,13 +2,16 @@ class StateMachine:
     def __init__(self, initalState):
         self.currentState = initalState
         self.previousState = initalState
+        self.inputs = []
         self.running = True
         
-    def runAll(self, inputs):
+    def runAll(self):
         self.currentState.run()
         while self.running:
             self.previousState = self.currentState
-            self.currentState = self.currentState.next(inputs.pop())
-            print(self.currentState)
+            self.currentState = self.currentState.next(self.inputs.pop())
             self.currentState.run()
+
+    def addInput(self, input):
+        self.inputs.append(input)
 
--- catkin_ws/src/speach/src/WaitingTask.py
+++ catkin_ws/src/speach/src/WaitingTask.py
@@ -1,21 +1,18 @@
 from State import State
 from StateMachine import StateMachine
-from Speech import speech, navigate
+from Speech import speech, navigate, listen
 
 
-inputs = []
 instance = 0
 
 class AskBooking(State):
+    global instance
     def run(self):
         speech("Do you have a booking")
-        response = 'no'
-        #response = listen()
-        global inputs
-        inputs.append(response)
+        response = listen()
+        instance.addInput(response)
 
     def next(self, input):
-        print(input)
         if 'yes' in input:
             return WaitingTask.bookingDetails
         if 'no' in input:
@@ -23,89 +20,73 @@ class AskBooking(State):
         return WaitingTask.unknowAnswer
 
 class AskGroupSize(State):
+    global instance
     def run(self):
         speech("How many people are their in your group")
-        response = 4
-        #response = listen()
-        global inputs
-        inputs.append(response)
+        response = listen()
+        instance.addInput(response)
 
     def next(self, input):
-        global instance
         amount = filter(lambda x: x.isdigit(), str(input))
         if amount == '':
             return WaitingTask.unknowAnswer
         else:
-            print('people ' + str(amount))
             instance.groupSize = int(amount)
             return WaitingTask.checkGroup
 
 class GuideToTable(State):
+    global instance
     def run(self):
-        global instance
         speech("Please follow me to your table")
         navigate("table"+str(instance.groupTable))
         speech("Please take a seat someone will be with you in a few minutes")
-        global inputs
-        inputs.append('')
-        global instance
+        instance.addInput('')
         instance.running = False
 
 class GiveWaitingTime(State):
+    global instance
     def run(self):
         speech("Your wait time is 60 minutes, is this okay")
-        #response = listen()
-        response = 'yes'
-        global inputs
-        inputs.append(response)
+        response = listen()
+        instance.addInput(response)
 
     def next(self, input):
         if 'yes' in input:
             return WaitingTask.guideToTable
         else:
-            print("Done")
-            global instance
             instance.running = False
 
 class BookingDetails(State):
+    global instance
     def run(self):
-        print("done")
-        global inputs
-        inputs.append('')
-        global instance
+        instance.addInput('')
         instance.running = False
 
 
 class CheckGroup(State):
+    global instance
     def run(self):
-        global instance
         for key in instance.tables:
             table = instance.tables[key]
             if table['avaliable'] and table['places'] >= instance.groupSize:
-                print(table)
                 table['avaliable'] = False
                 instance.groupTable = table['id']
-                print(table)
                 break
-        global inputs
-        inputs.append('')
-        print(instance.groupTable)
+        instance.addInput('')
 
     def next(self, input):
-        global instance
         if (instance.groupTable != -1):
             return WaitingTask.guideToTable
         else:
             return WaitingTask.giveWaitingTime
 
 class UnknownAnswer(State):
+    global instance
     def run(self):
-        speech("Sorry I didn't understand your answer, please can you repeat it")
-        global inputs
-        inputs.append('')
+        speech("Sorry I didn't understand your answer, please can you repeat that")
+        instance.addInput('')
         
     def next(self, inputs):
-        global instance
         return instance.previousState
 
 class WaitingTask(StateMachine):
@@ -135,5 +116,5 @@ WaitingTask.unknowAnswer = UnknownAnswer()
 WaitingTask.checkGroup = CheckGroup()
 
 instance = WaitingTask()
-instance.runAll(inputs)
+instance.runAll()
 
