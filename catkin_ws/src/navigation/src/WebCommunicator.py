2c80cec7a648daed50e1b9d96e78b51b86fbf102
Alex Banks <alex.banx98@gmail.com>
2019-12-03 17:35:53 +0000

    web interface for kitchen

--- catkin_ws/src/navigation/src/WebCommunicator.py
+++ catkin_ws/src/navigation/src/WebCommunicator.py
@@ -3,28 +3,45 @@ from flask import Flask
 from flask import request
 import navTo
 
+import taskManager
+
 app = Flask(__name__)
 
 locs = {
     'table1': {
-        'x' : 10,
+        'x': 10,
         'y': 20
     },
     'table2': {
         'x': 59,
-        'y':23
+        'y': 23
     }
 }
 
+
 @app.route('/')
 def hello():
+    print("hello")
     return 'Hello'
+
+
+@app.route('/kitchen')
+def kitchen():
+    print("kitchen")
+    try:
+        table = request.args.get('table', '')
+        taskManager.new_task("Deliver", table)
+    except:
+        print("error")
+        return "/kitchen?table=<table_number>"
+
 
 @app.route('/goto')
 def goto():
-    navTo.navigate(request.args.get('location',''))
+    # navTo.navigate(request.args.get('location',''))
     return 'going to ' + request.args.get('location', '') + str(locs[request.args.get('location', '')])
 
+
 if __name__ == '__main__':
-    navTo.main()
+    # navTo.main()
     app.run(host='0.0.0.0')
