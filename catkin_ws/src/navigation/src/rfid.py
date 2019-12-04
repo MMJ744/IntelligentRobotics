2451c6a601d9cb12b348a086236c440a24dae506
Matthew Jones <mattymjones@gmail.com>
2019-12-04 13:07:20 +0000

    added error check incase card reader isnt setup right

--- catkin_ws/src/navigation/src/rfid.py
+++ catkin_ws/src/navigation/src/rfid.py
@@ -14,14 +14,17 @@ from nfc.clf import RemoteTarget
 #Yellow = 76BBEB25
 #Black = 76DA3725
 def readCard():
-    customers = {'66411D25': 'Blank Boi', '66C8AA25': 'Brexit Means Brexit', '86C41A25': 'The Flying Scotsman', '76BBEB25': 'Barry Bee Benson', '76DA3725': 'Whatever you want'}
-    clf = nfc.ContactlessFrontend('usb')
-    tag = clf.connect(rdwr={'on-connect': lambda tag: False})
-    print(tag)
-    id = str(tag)[12::]
-    clf.close()
-    if id in customers:
-        return customers[id]
+    try:
+        customers = {'66411D25': 'Blank Boi', '66C8AA25': 'Brexit Means Brexit', '86C41A25': 'The Flying Scotsman', '76BBEB25': 'Barry Bee Benson', '76DA3725': 'Whatever you want'}
+        clf = nfc.ContactlessFrontend('usb')
+        tag = clf.connect(rdwr={'on-connect': lambda tag: False})
+        print(tag)
+        id = str(tag)[12::]
+        clf.close()
+        if id in customers:
+            return customers[id]
+    except:
+        print("SOMETHING WENT WRONG WITH CARD READING")
     return None
 
 print(readCard())
