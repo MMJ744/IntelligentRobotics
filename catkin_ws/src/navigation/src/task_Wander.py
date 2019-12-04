ce6c7c0b5fb6159af3da96dfaca0afbb29969da4
Matty Jones <mmj744@student.bham.ac.uk>
2019-12-04 21:05:56 +0000

    wander nav

--- catkin_ws/src/navigation/src/task_Wander.py
+++ catkin_ws/src/navigation/src/task_Wander.py
@@ -1,6 +1,6 @@
 from State import State
 from StateMachine import StateMachine
-from navController import navigateTo
+import navTo
 
 
 class CheckTables(State):
@@ -9,10 +9,10 @@ class CheckTables(State):
         tbls = map(lambda x: x["id"],filter(lambda tbl: not tbl["available"], instance.model.tables))
         tbls.sort(reverse=True)
         for table_int in tbls:
-            navigateTo("table" + str(table_int))
+            navTo.navigateTo("table" + str(table_int))    
             # if vision.areHumans:
-            #   self.model.tables[table]["available"] = True
-        navigateTo("frontdesk")
+            # self.model.tables[table]["available"] = True
+        navTo.navigateTo("frontdesk")
         # if vision.areHuman:
         taskManager.new_task("NewCust")
         taskManager.new_task("Wander")
@@ -21,6 +21,7 @@ class CheckTables(State):
 
 class WanderTask(StateMachine):
     def __init__(self, model):
+        navTo.main()
         StateMachine.__init__(self, CheckTables(), model) 
 
 
