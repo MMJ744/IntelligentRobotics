d061db404ddc3f2817c272526730c16c70b85aae
Brett Saunders <bjs730@student.bham.ac.uk>
2019-12-04 19:34:37 +0000

    Tested RFID

--- catkin_ws/src/navigation/src/rfid.py
+++ catkin_ws/src/navigation/src/rfid.py
@@ -18,7 +18,7 @@ def readCard():
         customers = {'66411D25': 'Blank Boi', '66C8AA25': 'Brexit Means Brexit', '86C41A25': 'The Flying Scotsman', '76BBEB25': 'Barry Bee Benson', '76DA3725': 'Whatever you want'}
         clf = nfc.ContactlessFrontend('usb')
         tag = clf.connect(rdwr={'on-connect': lambda tag: False})
-        print(tag)
+        #print(tag)
         id = str(tag)[12::]
         clf.close()
         if id in customers:
@@ -26,5 +26,3 @@ def readCard():
     except:
         print("SOMETHING WENT WRONG WITH CARD READING")
     return None
-
-print(readCard())
