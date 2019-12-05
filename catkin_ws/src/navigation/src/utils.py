9c7484b526957cff5fc8158b1ec77f893b7acf00
Brett Saunders <bjs730@student.bham.ac.uk>
2019-12-05 20:43:55 +0000

    Add more options

--- catkin_ws/src/navigation/src/utils.py
+++ catkin_ws/src/navigation/src/utils.py
@@ -34,9 +34,9 @@ def convertToNum(data):
 
 def getYesNo(data):
     tokens = data.split()
-    if 'yes' in tokens:
+    if 'yes' in tokens or 'yeah' in tokens or 'okay' in tokens':
         return 'yes'
-    if 'no' in tokens:
+    if 'no' in tokens or 'nah' in tokens:
         return 'no'
     return None
 
